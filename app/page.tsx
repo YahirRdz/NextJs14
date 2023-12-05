faaeaf60fb9500729b27a99bb79ce869bf60f93a
YahirRdz <71789382+YahirRdz@users.noreply.github.com>
2023-12-05 07:05:14 -0600

    Add Auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold"> Next 14 we´re comming</h1>
-      <h2 className="h2-bold"> Next 14 we´re comming</h2>
-      <h3 className="h3-bold"> Next 14 we´re comming</h3>
-    </div>
-  );
-}
