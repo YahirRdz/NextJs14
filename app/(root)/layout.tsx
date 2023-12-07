e4aa0d2dc84486f15d30377af6bd34e1ae023a5b
YahirRdz <71789382+YahirRdz@users.noreply.github.com>
2023-12-06 23:10:08 -0600

    Implementing Left side

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import LeftSide from "@/components/shared/LeftSide/LeftSide";
 import Navbar from "@/components/shared/navbar/Navbar";
 import React from "react";
 
@@ -6,11 +7,10 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <main className="background-light850_dark100 relative">
       <Navbar />
       <div className="flex ">
-        left side
+        <LeftSide />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        RightSideBar
       </div>
     </main>
   );
